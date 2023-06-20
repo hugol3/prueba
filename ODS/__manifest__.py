30d18d81347b69037f1f53837a34efcd884bd176
Josafat <josafatgarciasarmientos@gmail.com>
2023-06-20 17:19:20 -0600

    models done

--- ODS/__manifest__.py
+++ ODS/__manifest__.py
@@ -1,14 +1,21 @@
 {
     'name': "Mi Modulo",
     'version': '1.0',
-    'depends': ['base'],
+    'category': 'Sales/CRM',
+    'summary': 'Track groceeries to be delivered',
+    'depends': [
+        'base'
+    ],
     'author': "Author Name",
     'category': 'Category',
     'description': """
-    Description text
+        Track groceeries to be delivered
     """,
     # data files always loaded at installation
     'data': [],
     # data files containing optionally loaded demonstration data
     'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
 }
---
+++ ODS/models/__init__.py
@@ -0,0 +1,6 @@
+from . import beneficiario
+from . import categoria
+from . import inventario
+from . import orden_entrega
+from . import orden_producto
+from . import usuario
