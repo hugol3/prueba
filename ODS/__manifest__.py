4bcd49f32d6c731364a563b8934667c6d340e573
Josafat <josafatgarciasarmientos@gmail.com>
2023-06-20 14:04:01 -0600

    new module

--- ODS/__manifest__.py
+++ ODS/__manifest__.py
@@ -8,11 +8,7 @@
     Description text
     """,
     # data files always loaded at installation
-    # 'data': [
-    #     'views/mymodule_view.xml',
-    # ],
+    'data': [],
     # data files containing optionally loaded demonstration data
-    # 'demo': [
-    #     'demo/demo_data.xml',
-    # ],
+    'demo': [],
 }
