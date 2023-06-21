ec7be9ea7fd9d72e846c951dfa2b0680407c7a20
Goen345 <marlon.yahir.kiri@gmail.com>
2023-06-21 04:56:06 -0600

    redireccionar

--- ODS/models/inventario.py
+++ ODS/models/inventario.py
@@ -27,7 +27,7 @@ class Inventario(models.Model):
             'fecha_caducidad' : fecha_caducidad
         })
     def redireccionar(self):
-        view_id = self.env.ref('nombre_modulo.nombre_vista').id
+        view_id = self.env.ref('Mi Modulo.productos').id
         model = self._name
         url = '/web#id={}&view_type=form&model={}&action={}'.format(self.id, model, view_id)
         return http.request.redirect(url)
