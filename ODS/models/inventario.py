9a6414640104a7633e0eaf8134de3f43517a9f99
Goen345 <marlon.yahir.kiri@gmail.com>
2023-06-21 03:03:22 -0600

    Fecha inventario

--- ODS/models/inventario.py
+++ ODS/models/inventario.py
@@ -6,7 +6,7 @@ class Inventario(models.Model):
 
     id = fields.Integer()
     nombre = fields.Char()
-    fecha_recibido = fields.Date()
+    fecha_recibido = fields.Text()
     id_categoria = fields.Many2one("categoria.model", string="Categoria")
     id_usuario = fields.Many2one("usuario.model", string="Usuario")
     cantidad = fields.Integer()
