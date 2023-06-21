23f58864803b4f7f9b33722a506d5a827d86935e
Josafat <josafatgarciasarmientos@gmail.com>
2023-06-21 06:07:11 -0600

    some routing

--- ODS/controllers/inventary_operation.py
+++ ODS/controllers/inventary_operation.py
@@ -1,12 +1,34 @@
 from odoo import http
 from odoo.http import request
 
-class RegistrosController(http.Controller):
 
-    @http.route('/registros', auth='auth', website=True)
-    def registros(self, **kwargs):
-        registros = request.env['inventario.model'].sudo().search([])  # Obtén todos los registros
+class MyController(http.Controller):
 
-        return http.request.render('mimodulo.registros_template', {
-            'registros': registros,
-        })
+    @http.route('/my_module/registro', auth='user', website=True)
+    def create_record(self, **kwargs):
+        # Lógica para crear un nuevo registro
+        # Obtener los datos de la solicitud y guardarlos en un nuevo registro
+        # Devolver una respuesta adecuada (redirección, mensaje de éxito, etc.)
+        return 
+
+    @http.route('/my_module/registro/<int:id>', auth='user', website=True)
+    def read_record(self, id, **kwargs):
+        # Lógica para leer un registro existente
+        # Consultar el registro con el ID especificado
+        # Preparar y devolver una respuesta con los datos del registro
+        return
+
+    @http.route('/my_module/registro/<int:id>/editar', auth='user', website=True)
+    def update_record(self, id, **kwargs):
+        # Lógica para actualizar un registro existente
+        # Obtener los datos de la solicitud y aplicarlos al registro con el ID especificado
+        # Devolver una respuesta adecuada (redirección, mensaje de éxito, etc.)
+        return
+
+    @http.route('/my_module/registro/<int:id>/eliminar', auth='user', website=True)
+    def delete_record(self, id, **kwargs):
+        # Lógica para eliminar un registro existente
+        # Buscar el registro con el ID especificado y eliminarlo de la base de datos
+        # Devolver una respuesta adecuada (redirección, mensaje de éxito, etc.)
+        return "Hello"
+
