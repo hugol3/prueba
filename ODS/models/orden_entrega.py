8884383a1c108eaa49d039fe62540f52fa37e104
Josafat <josafatgarciasarmientos@gmail.com>
2023-06-21 04:08:42 -0600

    orden_view

--- ODS/models/orden_entrega.py
+++ ODS/models/orden_entrega.py
@@ -10,4 +10,12 @@ class Orden_entrega(models.Model):
     id_usuario = fields.Many2one("usuario.model",string="Usuario")
     id_beneficiario = fields.Many2one("beneficiario.model",string="Beneficiario")
 
+    def unlink(self):
+        return super(Orden_entrega, self).unlink()
     
+    def update_entrega(self, id,new_status, new_fecha):
+        entrega = self.env['entrega.model'].browse(id)
+        entrega.write({
+            'status': new_status,
+            'fecha': new_fecha,
+        })
